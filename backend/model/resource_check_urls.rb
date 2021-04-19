6b1183d26a50fe527887074f4297b0ce012746f2
crugas <corey.schmidt@uga.edu>
2021-04-19 09:26:52 -0400

    Initial commit

---
+++ backend/model/resource_check_urls.rb
@@ -0,0 +1,35 @@
+class CheckUrls < AbstractReport
+  register_report(
+    params: []
+  )
+
+  def query
+    results = db.fetch(query_string)
+    info[:total_count] = results.count
+    results
+  end
+
+  def query_string
+    <<~SOME_SQL
+      SELECT CONVERT(notes USING utf8) AS clean_notes FROM note HAVING instr(clean_notes, 'extref')
+    SOME_SQL
+  end
+
+  def page_break
+    false
+  end
+  # def notes
+  #   "SELECT CONVERT(notes USING utf8) AS clean_notes FROM note HAVING instr(clean_notes, 'extref')
+  #     "
+  # end
+  #
+  # def archival_object_titles
+  #   'SELECT * FROM archival_object LIMIT 10'
+  # end
+
+  def log(s)
+    Log.debug(s)
+    @job.write_output(s)
+  end
+
+end
