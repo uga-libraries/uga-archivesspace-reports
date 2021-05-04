82fd26cf4898784f4febbfd04b469ab40b6a0382
crugas <cschmidtuga@gmail.com>
2021-05-04 14:13:10 -0400

    Trying out regex matching, but keep getting error in local ASpace instance

--- backend/model/resource_check_urls.rb
+++ backend/model/resource_check_urls.rb
@@ -1,31 +1,58 @@
+# frozen_string_literal: true
+
+# require 'csv'
+# require 'uri'
+# require 'net/http'
+
 class ResourceCheckUrls < AbstractReport
   register_report(
-    params: []
+    params: [run_concurrently: true]
   )
 
-  def query
-    results = db.fetch(query_string)
-    info[:total_count] = results.count
-    results
+  def match_regex(text)
+    if text =~ %r[(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))]
+      puts text
+    end
   end
 
-  def query_string
+  def query
+    extref_results = db.fetch(extrefs)
+    info[:total_count] = extref_results.count
+    extref_results
+  end
+
+  def extrefs
     <<~SOME_SQL
       SELECT CONVERT(notes USING utf8) AS clean_notes FROM note HAVING instr(clean_notes, 'extref')
     SOME_SQL
   end
 
+  def notes
+    <<~SOME_SQL
+      SELECT CONVERT(notes USING utf8) as clean_notes FROM note
+    SOME_SQL
+  end
+
+  # def check_urls
+  #   notes_content = db.fetch(notes)
+  #   puts(notes_content)
+  #   notes_content.each do |result|
+  #     if result.include? 'content'
+  #       url_text = match_regex(result['content'])
+  #       # uri = URI(url_text)
+  #       # result = Net::HTTP.get_response(uri)
+  #       puts result unless (result = 200)
+  #     elsif result.include? 'subnotes'
+  #       result['subnotes'].each do |subnote|
+  #         url_text = match_regex(subnote['content']) if subnote.include? 'content'
+  #       end
+  #     end
+  #   end
+  # end
+
   def page_break
     false
   end
-  # def notes
-  #   "SELECT CONVERT(notes USING utf8) AS clean_notes FROM note HAVING instr(clean_notes, 'extref')
-  #     "
-  # end
-  #
-  # def archival_object_titles
-  #   'SELECT * FROM archival_object LIMIT 10'
-  # end
 
   def log(s)
     Log.debug(s)
